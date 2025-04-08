6786e7725ad14a98c9fa905f063c6f9ddb73a99e
angelgp-droid <{ID}+{username}@users.noreply.github.com>
2025-04-08 13:55:23 -0600

    make homepage as default landing page and remove defualt angular hello code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 export const routes: Routes = [
+    {path:'', component: HomepageComponent},
     {path:'about', component: AboutComponent}
 ];
