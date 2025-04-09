35bc27011f6038adf526e255b65acde9c8acd2a3
angelgp-droid <{ID}+{username}@users.noreply.github.com>
2025-04-09 16:13:38 -0600

    added counts to fix table generation when no input has been given

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -17,8 +17,24 @@ export class HomepageComponent {
   rowIsSubmitted = false;
   colIsSubmitted = false;
   colorIsSubmitted = false;
+  rowSubmitCount = 0;
+  colorSubmitCount = 0;
+  colSubmitCount = 0;
+
+  generateColorTable(){
+    if(this.colSubmitCount > 0){
+
+    }
+  }
+
+  generatePaintingTable(){
+    if(this.colSubmitCount > 1 && this.rowSubmitCount> 1){
+      
+    }
+  }
+
   isRowValid(): boolean {
-    if (!this.rowIsSubmitted) return true;
+    if (this.rowSubmitCount==0) return true;
     return this.rows !== null && 
            this.rows >= 1 && 
            this.rows <= 1000 &&
@@ -26,7 +42,7 @@ export class HomepageComponent {
   }
 
   isColValid(): boolean {
-    if (!this.colIsSubmitted) return true;
+    if (this.colSubmitCount==0) return true;
     return this.columns !== null && 
            this.columns >= 1 && 
            this.columns <= 702 &&
@@ -34,7 +50,7 @@ export class HomepageComponent {
   }
 
   isColorValid(): boolean {
-    if (!this.colorIsSubmitted) return true;
+    if (this.colorSubmitCount==0) return true;
     return this.color !== null && 
            this.color >= 1 && 
            this.color <= 10 &&
@@ -44,13 +60,18 @@ export class HomepageComponent {
 
   onRowSubmit() {
     this.rowIsSubmitted = true;
+    this.rowSubmitCount++;
   }
 
   onColSubmit() {
     this.colIsSubmitted = true;
+    this.colSubmitCount++;
   }
 
   onColorSubmit() {
     this.colorIsSubmitted = true;
+    this.colorSubmitCount++;
   }
+
+
 }
