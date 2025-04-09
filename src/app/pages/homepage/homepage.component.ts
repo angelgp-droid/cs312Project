bc0ce9a029ce594f27e98995650ed021b465812a
angelgp-droid <{ID}+{username}@users.noreply.github.com>
2025-04-09 15:55:05 -0600

    took away the else from validation ifs

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -17,35 +17,30 @@ export class HomepageComponent {
   rowIsSubmitted = false;
   colIsSubmitted = false;
   colorIsSubmitted = false;
-  isRowValid(): boolean{
+  isRowValid(): boolean {
     if (!this.rowIsSubmitted) return true;
-    else{
-      return this.rows !== null && 
-      this.rows >= 1 && 
-      this.rows <= 1000 &&
-      Number.isInteger(this.rows);
-    }
+    return this.rows !== null && 
+           this.rows >= 1 && 
+           this.rows <= 1000 &&
+           Number.isInteger(this.rows);
   }
 
-  isColValid(): boolean{
+  isColValid(): boolean {
     if (!this.colIsSubmitted) return true;
-    else{
-      return this.columns !== null && 
-      this.columns >= 1 && 
-      this.columns <= 702 &&
-      Number.isInteger(this.columns);
-    }
+    return this.columns !== null && 
+           this.columns >= 1 && 
+           this.columns <= 702 &&
+           Number.isInteger(this.columns);
   }
 
-  isColorValid(): boolean{
+  isColorValid(): boolean {
     if (!this.colorIsSubmitted) return true;
-    else{
-      return this.color !== null && 
-      this.color >= 1 && 
-      this.color <= 10 &&
-      Number.isInteger(this.color);
-    }
+    return this.color !== null && 
+           this.color >= 1 && 
+           this.color <= 10 &&
+           Number.isInteger(this.color);
   }
+
 
   onRowSubmit() {
     this.rowIsSubmitted = true;
