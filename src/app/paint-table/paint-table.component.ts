97270ecc47f0d91ab7cbaf26b2fcbfd5d3845c53
Erives Gutierrez <gman0770@lansing.cs.colostate.edu>
2025-04-12 13:50:38 -0600

    Full Color Painting Table Implementation

--- src/app/paint-table/paint-table.component.ts
+++ src/app/paint-table/paint-table.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-paint-table',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './paint-table.component.html',
   styleUrl: './paint-table.component.css'
 })
-export class PaintTableComponent {
+export class PaintTableComponent implements OnChanges{
+  @Input() rows: number = 0;
+  @Input() columns: number = 0;
 
+  columnLabels: string[] = [];
+
+  ngOnChanges(changes: SimpleChanges){
+    if (changes['columns']){
+      this.columnLabels = this.generateColumnLabels(this.columns);
+    }
+  }
+
+  generateColumnLabels(num: number): string[]{
+    let labels: string[] = [];
+    for(let i = 0; i < num; i++){
+      labels.push(this.toExcelColumn(i));
+    }
+    return labels;
+  }
+
+  toExcelColumn(n: number): string {
+    let result = '';
+    n = n + 1;
+    while(n > 0){
+      const remainder = (n - 1) % 26;
+      result = String.fromCharCode(65 + remainder) + result;
+      n = Math.floor((n - 1) / 26);
+    }
+    return result;
+  }
+
+  cellClicked(row: number, colIndex: number){
+    const colLabel = this.columnLabels[colIndex - 1];
+    alert(`${colLabel}${row}`);
+  }
+
+  createArray(length: number): number[] {
+    return Array.from({ length }, (_, i) => i);
+  }
 }
